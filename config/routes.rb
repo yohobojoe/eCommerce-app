84b4633b0b61e22924e922d24a8c3d4891754ccd
Alex Mandella <yohobojoe@gmail.com>
2018-03-15 09:11:36 -0500

    added a modal and fixed some routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+  get 'storefront/all_items'
+
+  get 'storefront/items_by_category'
+
+  get 'storefront/items_by_brand'
+
+  get 'categorical' => 'storefront#items_by_category'
+
+  get 'branding' => 'storefront#items_by_brand'
+
+  devise_for :users
   resources :orders
   resources :line_items
   resources :categories
   resources :products
 
-  root 'products#index'
+  root 'storefront#all_items'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
