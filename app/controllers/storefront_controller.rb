187adf4e9b530d4ebffa88199e67d4152f0d347b
Alex Mandella <yohobojoe@gmail.com>
2018-03-15 10:38:47 -0500

    navbar and productloop partial

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -4,8 +4,12 @@ class StorefrontController < ApplicationController
   end
 
   def items_by_category
+    @products = Product.where(category_id: params[:category_id])
+    @category = Category.find(params[:category_id])
   end
 
   def items_by_brand
+    @products = Product.where(brand: params[:brand])
+    @brand = params[:brand]
   end
 end
